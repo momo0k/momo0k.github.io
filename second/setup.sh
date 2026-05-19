aea6473efce7a7adfb63c4b311b31908294f053a
novembre <->
2026-05-19 15:43:28 +0000

    -

--- second/setup.sh
+++ second/setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# https://raw.githubusercontent.com/momo0k/momo0k.github.io/dac506f48a0e6d3f712c559485676dddf9099da9/sha256_ed25519.htmx
 PACK=(libreoff fire vlc samba)
 mkdir /var/log/setup 2>/dev/bul || apt update -y 1>> /dev/null 2>>/var/log/setup/warning_apt_update.log && echo -e 'Removing: \n ' ${PACK[@]}  '\n';
 for _o in ${PACK[@]}; do
